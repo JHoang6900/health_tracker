be2748eff0dd2baab4b278001902e1a7d7016812
JHoang <JHoang6900@gmail.com>
2024-03-28 02:58:01 -0600

    updated starting structure + dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,40 @@
-import {
-  ShaderGradientCanvas,
-  ShaderGradient,
-  // @ts-ignore
-  useThree,
-  // @ts-ignore
-  useFrame,
-} from 'shadergradient'
-import logo from './logo.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className='App'>
-      <ShaderGradientCanvas style={{ position: 'absolute' }}>
-        <Gradient />
-      </ShaderGradientCanvas>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
+    <>
+      <div className='flex items-center justify-center mb-6 text-xl'> {/* example of tailwind being used to modify app logos. */}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-      </header>
-    </div>
-  )
-}
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
 
-function Gradient() {
-  const { scene } = useThree()
 
-  useFrame(() => {
-    const mesh = scene.getObjectByName('shadergradient-mesh')
+      <p className="text-2xl italic read-the-docs text-rose-400">  {/* example of tailwind being used to override vanillacss. */}
+        This project has preinstalled tailwindcss ready to go! :)
+      </p>
 
-    if (mesh.material.userData.uNoiseStrength) {
-      mesh.material.userData.uNoiseStrength.value = 10
-    }
-  })
-
-  return (
-    <ShaderGradient
-      cDistance={32}
-      cPolarAngle={125}
-      color1='#ff5005'
-      color2='#dbba95'
-      color3='#d0bce1'
-    />
+    </>
   )
 }
 
